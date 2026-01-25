6f5f43e5503fd2852fc930cb21d4a4c5f78420a0
Brian Jorgensen <brian.jorgensen@gmail.com>
2026-01-25 09:18:19 -0700

    use Io in main

--- src/main.zig
+++ src/main.zig
@@ -3,13 +3,17 @@ const Io = std.Io;
 const Init = std.process.Init;
 
 pub fn main(init: Init) !u8 {
+    const arena: std.mem.Allocator = init.arena.allocator();
+    const name = "Phasor";
+    const greeting = try std.fmt.allocPrint(arena, "Hello, {s}!\n", .{name});
+
     const io = init.io;
 
     var stdout_buffer: [1024]u8 = undefined;
-    var stdout_file_writer: Io.File.Writer = .init(.stdout(), io, &stdout_buffer);
+    var stdout_file_writer: Io.File.Writer = .init(.stderr(), io, &stdout_buffer);
     const stdout_writer = &stdout_file_writer.interface;
 
-    try stdout_writer.print("Hello, Phasor!\n", .{});
+    try stdout_writer.print("{s}", .{greeting});
     try stdout_writer.flush();
     return 0;
 }
