8e21aa76373922e4c4ce3c1b3c9ef72287a54087
Brian Jorgensen <brian.jorgensen@gmail.com>
2026-01-30 00:45:11 -0700

    consolidate wasm module

--- lib/wasm/io.zig
+++
@@ -1,8 +0,0 @@
-const std = @import("std");
-
-const Io = std.Io;
-const Threaded = std.Io.Threaded;
-
-pub fn io() Io {
-    return Threaded.global_single_threaded.ioBasic();
-}
--- lib/wasm/root.zig
+++ lib/wasm/root.zig
@@ -1,1 +1,5 @@
-pub const io = @import("io.zig").io;
+const std = @import("std");
+
+pub fn io() std.Io {
+    return std.Io.Threaded.global_single_threaded.ioBasic();
+}
